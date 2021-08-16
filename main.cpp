6c3630cce926262305b52f7d4193a29386d5308b
Gustavo <gustavoddacorte@gmail.com>
2021-08-16 17:10:56 +0900

    made initial base structure for development, to bbe continued

--- main.cpp
+++ main.cpp
@@ -6,9 +6,60 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function declarations (not moving to a separate .h/.cpp file structure due to project simplicity
+vector<long> getNthTermRecursively(long *, int termIndex, int startingSize = 2,  long startingN = 0);
+vector<long> getNthTermIteratively(long *, int termIndex, int startingSize = 2,  long startingN = 0);
+long getNthTermDirectly(int termIndex);
+long calculateNextFibonacciTerm(long previous, long current);
+
+const double GOLDEN_NUMBER = 1.618034;
 
 int main(int argc, const char * argv[]) {
+    //initial sequence will always have length of two, we assume here it starts with the term F0, F1. This can be adjusted
+    long initialSeq[] = {0, 1};
+    int termIndex;
+    vector<long> termValueRecursive,
+                 termValueIteractive;
+    long termValueDirect;
+    
     // insert code here...
-    std::cout << "Hello, Worldasdcsa!\n";
+    cout << "Please type desired n-th term (and press enter): ";
+    cin >> termIndex;
+    
+    termValueRecursive = getNthTermRecursively(initialSeq, termIndex);
+    termValueIteractive = getNthTermIteratively(initialSeq, termIndex);
+    termValueDirect = getNthTermDirectly(termIndex);
+    
+    cout << "The n-th term is (recusrsive approach): " << termValueRecursive.back() << "\n";
+    cout << "The n-th term is (iteractive approach): " << termValueIteractive.back() << "\n";
+    cout << "The n-th term is (direct approach): " << termValueDirect << "\n";
+    
     return 0;
+}
+
+// Functions implementation
+vector<long> getNthTermRecursively(long * initialSeq, int termIndex,  int startingSize,  long startingN) {
+    vector<long> sequence(initialSeq, initialSeq+startingSize);
+    
+    return sequence;
+}
+
+vector<long> getNthTermIteratively(long * initialSeq, int termIndex, int startingSize,  long startingN) {
+    vector<long> sequence(initialSeq, initialSeq+startingSize);
+    
+    return sequence;
+}
+
+long getNthTermDirectly(int termIndex) {
+    
+    return long(GOLDEN_NUMBER);
+}
+
+long calculateNextFibonacciTerm(long previous, long current) {
+    
+    return 91823;
 }
